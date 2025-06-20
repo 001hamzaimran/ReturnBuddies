431f9b338d027f79fe228af296e4f68b8e81cd7c
001hamzaimran <syed.hamza2004@gmail.com>
2025-06-20 18:12:29 +0500

    Set Mongoose

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
 
-const DbCon=async()=>{
+const DbCon = async () => {
     try {
-        const conn=await mongoose.connect(process.env.Db_Url)
+        const conn = await mongoose.connect(process.env.Db_Url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 60000,
+            socketTimeoutMS: 60000,
+        })
         console.log(`Database connected successfully`);
     } catch (error) {
         console.log(`Error while connecting with database ${error}`);
