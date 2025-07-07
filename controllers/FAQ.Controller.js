f0bb9ca176da7764e2d68e8e4ec2fa00d8a7e3d7
001hamzaimran <syed.hamza2004@gmail.com>
2025-07-07 18:58:52 +0500

    Label receipt

--- controllers/FAQ.Controller.js
+++ controllers/FAQ.Controller.js
@@ -5,10 +5,10 @@ const createFAQ = async (req, res) => {
         const { question, answer } = req.body;
         const faq = new FAQ({ question, answer });
         await faq.save();
-        res.status(200).json({ message: "FAQ created successfully", faq });
+        res.status(200).json({ message: "FAQ created successfully", faq, status: 200 });
     } catch (error) {
         console.error("Error creating FAQ:", error);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ message: "Internal server error", status: 500 });
     }
 }
 
@@ -16,10 +16,10 @@ const createFAQ = async (req, res) => {
 const getAllFAQ = async (req, res) => {
     try {
         const faqs = await FAQ.find();
-        res.status(200).json({ data: faqs, success: true });
+        res.status(200).json({ data: faqs, success: true, status: 200 });
     } catch (error) {
         console.error("Error fetching FAQs:", error);
-        res.status(500).json({ error: "Internal server error", success: false });
+        res.status(500).json({ error: "Internal server error", success: false, status: 500 });
     }
 };
 
