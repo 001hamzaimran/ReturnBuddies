5e0d72f6b7bf19d29fadb1d66ca0a1c3fecbc0ed
001hamzaimran <syed.hamza2004@gmail.com>
2025-07-17 17:26:14 +0500

    add promo code API's

--- controllers/Promo.Controller.js
+++ controllers/Promo.Controller.js
@@ -28,16 +28,16 @@ const getPromo = async (req, res) => {
         const discountCode = req.query.code || req.params.code;
 
         if (!userid) {
-            return res.status(400).json({ message: "User ID is required in headers", success: false });
+            return res.status(200).json({ message: "User ID is required in headers", success: false,status: 400 });
         }
 
         if (!discountCode) {
-            return res.status(400).json({ message: "Promo code is required in query or param", success: false });
+            return res.status(200).json({ message: "Promo code is required in query or param", success: false ,status: 400 });
         }
 
         const user = await UserModel.findById(userid);
         if (!user) {
-            return res.status(404).json({ message: "User not found", success: false });
+            return res.status(200).json({status: 404, message: "User not found", success: false });
         }
 
         const promo = await PromoCodeModal.findOne({
@@ -50,10 +50,10 @@ const getPromo = async (req, res) => {
         });
 
         if (!promo) {
-            return res.status(404).json({ message: "Promo code not found or not available for this user", success: false });
+            return res.status(200).json({status: 404, message: "Promo code not found or not available for this user", success: false });
         }
 
-        return res.status(200).json({ message: "Promo code found", success: true, promo });
+        return res.status(200).json({status: 200, message: "Promo code found", success: true, promo });
     } catch (error) {
         console.error('getPromo error:', error);
         return res.status(500).json({ message: "Internal Server Error", success: false });
