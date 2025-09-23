b86ba0b21ad2b9092a44caa4db7c3540d0a28e00
Farzam <frzamn64ml@gmail.com>
2025-09-23 21:02:18 +0500

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,7 +1,4 @@
 import sgMail from "@sendgrid/mail";
 
-// sgMail.setApiKey(process.env.SendGrid_API_Key);
-sgMail.setApiKey(
-  "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"
-);
+sgMail.setApiKey(process.env.SendGrid_API_Key);
 export default sgMail;
