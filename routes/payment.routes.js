f9291e1f005abb70ad4102df9408d008f22a1611
Farzam <frzamn64ml@gmail.com>
2025-11-13 16:29:16 +0500

    Update payment.routes.js

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -9,4 +9,4 @@ PaymentRouter.post("/edit-Payment-card/:cardId", isLogin, editCard);
 PaymentRouter.get("/get-Payment-card", isLogin, getUserCards);
 PaymentRouter.post("/delete-Payment-card/:Id", isLogin, deleteCard);
 PaymentRouter.get("/get-All-Payment", getAllPayments);
-PaymentRouter.post("create-or-get-customer", createOrGetCustomer);
+PaymentRouter.post("/create-or-get-customer", createOrGetCustomer);
